a78015f05291fa988fad022281d8cada968f4096
jvitorn <joaoeuno2011@gmail.com>
2020-05-08 10:57:58 -0300

    Server - Await/Async em Mongoose

--- api/models/usuario.js
+++ api/models/usuario.js
@@ -1,101 +1,98 @@
 const mongoose = require('mongoose');
 
 //model
-// const Usuario = new UsuarioCollection;
 
 class UsuarioDao{
 
     // é enviado esses parametros de usuario pelo cliente, apos isso é inserido
-    criarUsuario(usuario,res){
+    async criarUsuario(usuario,res){
         
         //informações para cadastrar na collection
         const usuarioNome = usuario.nome;
         const usuarioEmail = usuario.email;
         const usuarioSenha = usuario.senha;
         const Usuario = mongoose.model('usuarios');
-        //save -> insert
-        const inserir = new Usuario ({
-            nome:usuarioNome,
-            email:usuarioEmail,
-            senha:usuarioSenha
-        }).save();
+       
+        try {
+             //save -> insert
+            const inserir = await new Usuario ({
+                nome:usuarioNome,
+                email:usuarioEmail,
+                senha:usuarioSenha
+            }).save();
 
-        inserir.then((results)=>{
-            console.log("Cadastro de Novo Usuario efetuado");
-            res.status(201).json({msg:"Cadastro de Novo Usuario efetuado",id:results._id});
-        })
-        .catch((error)=>{
+            res.status(201).json({msg:"Cadastro de Novo Usuario efetuado",id:inserir._id});
+        }
+        catch(error){
             res.status(404).json({msg:"Erro ao cadastrar um novo usuario ao banco de dados",error})
-        })
+        }
     }
-    listar(res){
+    async listar(res){
         const Usuario = mongoose.model('usuarios'); 
-        //mostrando todos os usuarios
-        Usuario.find().sort({nome:1})
-        .then((usuarios)=>{
-            //passando status e um json com a resposta
-            res.status(202).json(usuarios);
-        })
-        .catch((error)=>{
-            res.status(404).json({msg:"Erro ao listar usuarios ao banco de dados",error})
-        })
+            try{
+                 //find
+                const find = await Usuario.find().sort({nome:1}).exec();
+                res.status(202).json(find);
+            }catch(err){
+                res.status(404).json({msg:"Erro ao listar usuario ao banco de dados",err})
+            }   
     }
-    logarUsuario(usuario,res){
+    async logarUsuario(usuario,res){
         //Collection de Usuarios
         const Usuario = mongoose.model('usuarios');
         //Função de procurar Id e enviar os dados daquele Usuario
         const id  = usuario.id;
-
-        Usuario.findOne( { _id:id } )
-        .then((results)=>{
-            res.status(202).json({nome:results.nome});
-        })
-        .catch((error)=>{
+       
+        try{
+            const find = await Usuario.findOne( { _id:id } )
+            res.status(202).json(find);
+        }
+        catch(error){
             res.status(400).json({msg:"Erro ao logar usuario,favor tente novamente!",error});
-        })
+        }
 
     }
-    atualizar(id,valores,res){
+    async atualizar(id,valores,res){
         //Collection de Usuarios
         const Usuario = mongoose.model('usuarios');
-        //atualizar informações
-        Usuario.updateOne({_id:id},{
-            nome:valores.nome,
-            email:valores.email
-        })
-        .then((results)=>{
-            res.status(201).json({msg:"Dados Atualizados Com Sucesso",results});
-        })
-        .catch((error)=>{
+        try{
+             //atualizar informações
+            const find = await Usuario.updateOne({_id:id},{
+                nome:valores.nome,
+                email:valores.email
+            })
+            res.status(201).json({msg:"Dados Atualizados Com Sucesso",find});
+        }
+        catch(error){
             res.status(400).json({msg:"Erro ao atualizar usuario ao banco de dados",error});
-        })
+        }
         //Função de 
     }
-    deletarUsuario(id,res){
+    async deletarUsuario(id,res){
         //Collection de Usuarios
         const Usuario = mongoose.model('usuarios');
-        //Função de remover usuario passando como parametro o ID dele 
-        Usuario.deleteOne({ _id:id })
-        .then((usuario)=>{
+        try{
+            //Função de remover usuario passando como parametro o ID dele 
+            const deleteUser = await Usuario.deleteOne({ _id:id })
             //passando status e um json com a resposta
-            res.status(204).json({msg:"Usuario Removido do Banco de Dados"});
-        })
-        .catch((error)=>{
+            res.status(204).json({msg:"Usuario Removido do Banco de Dados",deleteUser});
+        }
+        catch(error){
             res.status(400).json({msg:"Erro ao deletar o  usuario ao banco de dados",error});
-        })
+        }
     }
-    localizarId(usuario,res){
+    async localizarId(usuario,res){
         const email = usuario.email;
         //Collection de Usuarios
         const Usuario = mongoose.model('usuarios');
-        //atualizar informações
-        Usuario.findOne({email:email})
-        .then((results)=>{
-            res.status(200).json(results);
-        })
-        .catch((error)=>{
+        try{
+            //atualizar informações
+            const find = await Usuario.findOne({email:email})
+            res.status(200).json(find);
+        }
+        catch(error){
             res.status(400).json({msg:"Erro ao localizar usuario",error});
-        })
+        }
 
     }
 }
